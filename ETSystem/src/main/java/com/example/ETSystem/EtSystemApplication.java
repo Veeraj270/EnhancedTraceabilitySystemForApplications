23c00109b0d319dc435419c5ccbee8052ed8f1c4
Matthew Cudby <oh22896@bristol.ac.uk>
2023-11-08 21:12:57 +0000

    Spring-boot skeleton code from spring-initializr added with the following dependencies: Spring Web, PostgreSQL Driver, Spring Data JPA

---
+++ ETSystem/src/main/java/com/example/ETSystem/EtSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.ETSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtSystemApplication.class, args);
+	}
+
+}
---
+++ ETSystem/src/test/java/com/example/ETSystem/EtSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ETSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
