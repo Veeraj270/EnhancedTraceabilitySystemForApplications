3cf6719e6cbb7b71401cfd2abc0064f92e77288d
Matthew Cudby <oh22896@bristol.ac.uk>
2024-01-26 13:24:30 +0000

    Revert "Had to change front-end docker file slightly such that @tanstack/react-table gets installed, have made some progress in developing an updated version of the product table - significant additional work required."
    This reverts commit 9cdacf8dbfbadb805168b14f9aec975772a68b89.

--- et-system-frontend/src/Pages/ProductsPage.tsx
+++ et-system-frontend/src/Pages/ProductsPage.tsx
@@ -1,11 +1,11 @@
-import TanTable from "./ProductPageComponents/TanStackTable/TanTable";
+import BasicTable from './ProductPageComponents/Table/BasicTable'
 
 const ProductsPage = () => {
     return (
         <div className='page'>
             <h1>Products</h1>
             <h3>All Products:</h3>
-            <TanTable/>
+            <BasicTable/>
         </div>
     )
 }
