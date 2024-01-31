448a70ba7f79567946b80ee732a31bf2b1ccd9f1
Veeraj270 <ro22780@bristol.ac.uk>
2024-01-31 09:20:19 +0000

    first working version. implement dependencies and editing

--- ETSystem/src/main/java/com/example/ETSystem/product/ProductAPI.java
+++ ETSystem/src/main/java/com/example/ETSystem/product/ProductAPI.java
@@ -1,15 +1,16 @@
 package com.example.ETSystem.product;
 
+import com.example.ETSystem.timeline.TimelineData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
 @RestController
 @RequestMapping(path = "api/products")
 @CrossOrigin(origins = "http://localhost:3000")
-public class ProductAPI {
+public class ProductAPI{
+
     private final ProductService productService;
 
     @Autowired
@@ -22,23 +23,17 @@ public class ProductAPI {
         return productService.getProducts();
     }
 
-
     @GetMapping(path = "/fetch-product-intermediaries/{id}")
-    public List<Product> getProductIntermediaries(@PathVariable("id") String id){
-        //NEEDS WORK
-        System.out.println("getProductIntermediaries request received");
-        System.out.println(id);
-
-        Long ID_long = Long.parseLong(id);
-        List<Product> list =  productService.getProductIntermediaries(ID_long);
-        for (Product product : list){
-            System.out.println(product.getParentID());
-        }
-        return list;
+    public List<Product> getProductIntermediaries(@PathVariable("id") long id){
+        return productService.getProductIntermediaries(id);
     }
 
+    @GetMapping(path = "/fetch-product-history/{id}")
+    public List<TimelineData> fetchProductHistory(@PathVariable("id") long id){
+        return productService.getProductHistory(id);
+    }
 
-    @PostMapping(path = "/add")
+    @PostMapping(path = "/add-product")
     public Product addProduct(@RequestBody Product newProduct){
         productService.addNewProduct(newProduct);
         return newProduct;
