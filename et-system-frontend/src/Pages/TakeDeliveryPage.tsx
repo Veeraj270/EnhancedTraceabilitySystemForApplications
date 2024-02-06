39f4ad85cd17a1fd0f42c15a5c24e587ded5445b
Matthew Cudby <oh22896@bristol.ac.uk>
2024-02-06 22:36:23 +0000

    Expected items that are scanned are now correctly removed from the Expected Items table and added to the Scanned Items Table

--- et-system-frontend/src/Pages/TakeDeliveryPage.tsx
+++ et-system-frontend/src/Pages/TakeDeliveryPage.tsx
@@ -54,22 +54,16 @@ const TakeDelivery = () => {
         })
     }, []);
 
-    //Debugging
-    useEffect(() => {
-        console.log(expectedTData)
-    }, [expectedTData]);
-
-
     const fetchDeliveryData = async (deliveryId: number) : Promise<Delivery> => {
         try {
             const response = await fetch(`http://localhost:8080/api/deliveries/fetch-planned-by-id/${deliveryId}`);
             if (!response.ok) {
-                throw new Error('Network response was not ok');
+                throw new Error('fetch-planned-by-id response was not ok');
             }
             const data = await response.json();
             return data;
         } catch (error) {
-            console.error("Error occurred as a result of fetch-planned-by-id", error);
+            console.error("Error: ", error);
             return {
                 name: "error",
                 deliveryInterval: "error",
@@ -81,48 +75,37 @@ const TakeDelivery = () => {
         }
     }
 
-    //Fetch product data
-    const fetchProductLabel = async (barcode: string) => {
-        const labelKeys = ['generic_name', 'product_name', 'name', 'title', 'label'];
-        const res = await fetch(`https://world.openfoodfacts.org/api/v2/product/${barcode}.json`)
-        if (!res.ok){
-            throw new Error(`openfoodfacts.org API query with barcode: ${barcode} response was not ok`)
-        }
-        let resJSON = await res.json();
-
-        //Try each labelKey
-        for (const key of labelKeys){
-            if(resJSON.product && resJSON.product[key]){
-                return resJSON.product[key];
-            }
-        }
-        return "Unknown"
-    }
 
     //Triggered by pressing button to right of input field or by pressing enter on input field
     const submitBarcode = async (barcode: string) => {
-        let productLabel = "Unknown";
-        try {
-            productLabel = await fetchProductLabel(barcode);
-        } catch(error){
-            console.log("Error occurred within fetchProductLabel(): " + error);
-        }
-        const item : DeliveryItem = {
-            name: productLabel,
-            gtin: barcode,
+        console.log("barcode: ", barcode)
+        for (let i = 0; i < expectedTData.length; i ++){
+            if (expectedTData[i].gtin == barcode){
+                console.log("barcode found in expectedTData")
+                setScannedTData([expectedTData[i],...scannedTData]);
+                //Create shallow copy
+                const newExpectedTData = expectedTData.slice()
+                //Remove item
+                newExpectedTData.splice(i,1)
+                //Update expectedTData array
+                setExpectedTData(newExpectedTData)
+                break;
+            }
         }
 
-        //Search expectedTData for barcode
-        if (expectedTData.some(item => item.gtin === barcode)){
-            //Remove from expectedTData
-            setExpectedTData(expectedTData.filter((item: DeliveryItem) => (item.gtin !== barcode)));
-            //Add to expectedTData
-            setScannedTData((prevState) => [item, ...prevState])
-        }
-        else {
-            //Item is unexpected
-            setUnexpectedTData((prevState) => [item, ...prevState]);
-        }
+        //If barcode isn't within expectedTData query back-end API
+       /* const label = "Unknown";
+        try {
+            const response = await fetch(`http://localhost:8080/api/lookup/barcode/lookup-by-gtin/${barcode}`);
+            if (!response.ok) {
+                throw new Error('lookup/barcode/lookup-by-gtin response was not ok');
+            }
+            const responseJSON = await response.json();
+            console.log(responseJSON);
+
+        } catch (error) {
+            console.log("Error: ", error);
+        }*/
     }
 
     //Triggered by pressing submit delivery button
