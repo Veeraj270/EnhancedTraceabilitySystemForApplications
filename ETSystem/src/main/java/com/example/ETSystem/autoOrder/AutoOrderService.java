7b524912fe53c35d5f87dcb400af442500e5a940
josif <qc22435@bristol.ac.uk>
2024-02-17 17:16:53 +0000

    Method for getting ingredients and quantities from a FinalProduct.

--- ETSystem/src/main/java/com/example/ETSystem/autoOrder/AutoOrderService.java
+++ ETSystem/src/main/java/com/example/ETSystem/autoOrder/AutoOrderService.java
@@ -1,7 +1,14 @@
 package com.example.ETSystem.autoOrder;
 
+import com.example.ETSystem.finalProducts.FinalProduct;
+import com.example.ETSystem.recipe.Ingredient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.AbstractMap.SimpleEntry;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 @Component
 public class AutoOrderService {
@@ -9,6 +16,14 @@ public class AutoOrderService {
     @Autowired
     public AutoOrderService(){
 
+    }
+
+    public List<Entry<Ingredient, Integer>> getIngredients(FinalProduct finalProduct){
+        return finalProduct.getRecipe()
+                .getIngredients()
+                .stream()
+                .map(x -> new SimpleEntry<>(x.getIngredient(), x.getQuantity()))
+                .collect(Collectors.toList());
     }
 
 }
