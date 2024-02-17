936fb38d955816a52dc26f45a9a47a8a37fdd60d
josif <qc22435@bristol.ac.uk>
2024-02-17 20:41:22 +0000

    Added Recipe interface. Made the table with data in RecipeTable. Updated RPStylesheet.

---
+++ et-system-frontend/src/Pages/Interfaces/Recipe.tsx
@@ -0,0 +1,7 @@
+export interface Recipe{
+    id: number;
+    label: string;
+    allergens: string[];
+    vegan: boolean;
+    vegetarian: boolean;
+}
--- et-system-frontend/src/Pages/RecipePageComponents/RecipeTable.tsx
+++ et-system-frontend/src/Pages/RecipePageComponents/RecipeTable.tsx
@@ -1,22 +1,62 @@
-import {useState} from "react";
+import React, {ChangeEvent, useMemo, useState} from "react";
+import {flexRender, getCoreRowModel, useReactTable} from "@tanstack/react-table";
+import {Recipe} from "../Interfaces/Recipe";
 
 const RecipeTable = () => {
 
+    const [tableData, setTableData] = useState([])
     const [searchInput, setSearchInput] = useState("")
 
-    const handleChange = (event : any) => {
+    const columns = useMemo(() => [
+        {
+            header: 'ID',
+            accessorKey: 'id'
+        },
+        {
+            header: 'Name',
+            accessorKey: 'name'
+        }
+    ], [])
+
+    const generateTableData = async (data: any[]) => {
+        let tableData: any[] = [];
+        data.map((recipe: Recipe) => {
+            tableData.push({
+                id: recipe.id,
+                label: recipe.label
+            })
+            setTableData(tableData)
+        })
+    }
+
+    const handleChange = (event : ChangeEvent<HTMLInputElement>) => {
         event.preventDefault();
         setSearchInput(event.target.value)
     }
 
-    return <div className={'RPTable-grid'}>
-        <div className={'RPTable-search-container'}>
+    const table = useReactTable({
+        data: tableData,
+        columns: columns,
+        getCoreRowModel: getCoreRowModel()
+    })
+
+    return <div className={"RPTable-grid"}>
+        <div className={"RPTable-search-container"}>
             <label>Search recipes</label>
             <input placeholder={"Search... "} onChange={handleChange} value={searchInput}/>
         </div>
 
-        <div className={'DOP-T-content-div'}>
+        <div className={"RPTable-content-div"}>
+            <table>
+                <tbody>
 
+                        {/*{table.columns.map(column => (*/}
+                        {/*    <th key={column.id} style={{ width: `${column.column.getSize()}%`, textAlign: "center" }}>*/}
+                        {/*        {flexRender(column.columnDef.header, column.getHeaderProps())}*/}
+                        {/*    </th>*/}
+                        {/*))}*/}
+                </tbody>
+            </table>
         </div>
     </div>
 }
