74cd70dceb26e4c977a02bed8fe09df50efc4090
josif <qc22435@bristol.ac.uk>
2024-02-23 14:45:59 +0000

    Restructured the page in 3 columns and added a stylesheet.

--- et-system-frontend/src/Pages/AddRecipePage.tsx
+++ et-system-frontend/src/Pages/AddRecipePage.tsx
@@ -1,23 +1,71 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import AddRecipe from "./AddRecipePageComponents/AddRecipe";
 import IngredientQuantitiesTable from "./RecipePageComponents/IngredientQuantitiesTable";
+import RecipeTable from "./RecipePageComponents/RecipeTable";
+import "./AddRecipePageComponents/ARPStylesheet.css"
+import IngredientQuanitytPanel from "./AddRecipePageComponents/IngredientQuanitytPanel";
+import {IngredientQuantity} from "./Interfaces/IngredientQuantity";
 
 const AddRecipePage = () => {
 
+    const [selectedIngredient, setSelectedIngredient] = useState(-1)
+    const [ingredientsData, setIngredientsData] = useState([])
+    const [panelProps, setPanelProps] = useState({})
     const [recipe, setRecipe] = useState({
         label: '',
         ingredients: []
     })
 
+    const fetchRecipes = async () => {
+        const response = await fetch("http://localhost:8080/api/recipes/fetch-ingredients")
+        if (!response.ok){
+            throw new Error("Error fetching recipes")
+        }
+        return await response.json();
+    }
+
+    useEffect(() => {
+        fetchRecipes().then((ingredientData) =>
+        {
+            setIngredientsData(ingredientData)
+        })
+            .catch((reason) => {console.error("Error within fetchIngredients" + reason)})
+    }, [])
+
+    useEffect(() => {
+        let panelPropsData = {}
+        if (selectedIngredient !== -1){
+            const selected = ingredientsData.filter((ingredient) => ingredient.id === selectedIngredient).at(0)
+            panelPropsData = selected
+            console.log(selected);
+        }
+        setPanelProps(panelPropsData)
+    }, [selectedIngredient]);
+
     return(
-        <div>
-            <AddRecipe
-                recipe = {recipe}
-                setRecipe = {setRecipe}
-            />
-            <IngredientQuantitiesTable
-                ingredientQuantities={recipe.ingredients}
-            />
+        <div className={'add-recipe-page'}>
+            <div>
+
+            </div>
+            <div className={'ARP-grid-container'}>
+                <div className={'ARP-grid-column'}>
+                    <RecipeTable
+                        setSelectedRow={setSelectedIngredient}
+                        selectedRow={selectedIngredient}
+                        rawData={ingredientsData}
+                        />
+                </div>
+                <div className={'ARP-grid-column'}>
+                    <IngredientQuanitytPanel
+                        props={panelProps}
+                    />
+                </div>
+                <div className={'ARP-grid-column'}>
+                    <IngredientQuantitiesTable
+                        ingredientQuantities={recipe.ingredients}
+                    />
+                </div>
+            </div>
         </div>
     )
 
