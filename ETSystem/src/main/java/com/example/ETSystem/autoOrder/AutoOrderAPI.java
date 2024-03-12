e229d8bcae1278ee5d73255694baab6cd3c329be
Matthew Cudby <oh22896@bristol.ac.uk>
2024-03-12 20:31:54 +0000

    getAutoGenOrdersPlus now returns both the planned orders and the products in the product repo that can be used to fulfill the order

--- ETSystem/src/main/java/com/example/ETSystem/autoOrder/AutoOrderAPI.java
+++ ETSystem/src/main/java/com/example/ETSystem/autoOrder/AutoOrderAPI.java
@@ -2,6 +2,7 @@ package com.example.ETSystem.autoOrder;
 
 import com.example.ETSystem.customerOrders.CustomerOrder;
 import com.example.ETSystem.deliveries.PlannedDelivery;
+import com.example.ETSystem.product.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,11 +29,17 @@ public class AutoOrderAPI {
     }
 
     @GetMapping(path = "/auto-gen-orders-plus")
-    public List<PlannedDelivery> getAutoGenOrdersPlus(@RequestBody CustomerOrder order){
+    public AutoOrderGenOrderPlusResponse getAutoGenOrdersPlus(@RequestBody CustomerOrder order){
         autoOrderService.setSavedOrder(order);
+
+        //Generate the planned deliveries that represent the orders to suppliers
         List<PlannedDelivery> planned = autoOrderService.generateRequiredOrders(order, true);
+
+        //Save the deliveries - so they can be saved upon confirmation
         autoOrderService.setSavedDeliveries(planned);
-        return planned;
+
+        //Send back both the planned orders to suppliers, and the products in the Product repo that can be used to fulfill the requirements of the order
+        return new AutoOrderGenOrderPlusResponse(planned, autoOrderService.getSavedProducts());
     }
 
 
@@ -45,7 +52,6 @@ public class AutoOrderAPI {
         autoOrderService.setSavedDeliveries(null);
         autoOrderService.setSavedProducts(null);
         autoOrderService.setSavedOrder(null);
-
     }
 
     @RequestMapping(path = "/auto-gen-orders/cancel")
@@ -55,5 +61,16 @@ public class AutoOrderAPI {
         autoOrderService.setSavedProducts(null);
         autoOrderService.setSavedOrder(null);
         return false;
+    }
+
+    //Custom response type
+    public class AutoOrderGenOrderPlusResponse{
+        public List<PlannedDelivery> plannedOrders;
+        public List<Product> productsToReserve;
+
+        public AutoOrderGenOrderPlusResponse(List<PlannedDelivery> plannedOrders, List<Product> productsToReserve){
+            this.plannedOrders = plannedOrders;
+            this.productsToReserve = productsToReserve;
+        }
     }
 }
