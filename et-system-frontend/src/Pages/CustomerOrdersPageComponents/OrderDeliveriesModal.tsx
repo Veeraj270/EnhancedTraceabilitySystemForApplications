31af55918e8479b39250c5e04dfe0a13898a08e0
Veeraj270 <ro22780@bristol.ac.uk>
2024-03-17 04:18:01 +0000

    List is now concise

--- et-system-frontend/src/Pages/CustomerOrdersPageComponents/OrderDeliveriesModal.tsx
+++ et-system-frontend/src/Pages/CustomerOrdersPageComponents/OrderDeliveriesModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 
 type ModalProps = {
     plannedDeliveries: PlannedDelivery[];
@@ -21,17 +21,37 @@ type DeliveryItem = {
     gtin: string;
 }
 
+
+
 const OrderDeliveriesModal: React.FC<ModalProps> = ({plannedDeliveries, onConfirm, onCancel }) => {
+
+    const aggregatedDeliveryItems = useMemo(() => plannedDeliveries.map(delivery => {
+        const labelCount = delivery.items.reduce((acc, item) => {
+            acc[item.label] = (acc[item.label] || 0) + 1;
+            return acc;
+        }, {});
+
+        const aggregatedItems = Object.entries(labelCount).map(([label, count]) => ({
+            label,
+            quantity: count,
+        }));
+
+        return {
+            ...delivery,
+            items: aggregatedItems,
+        };
+    }), [plannedDeliveries]);
+
     return(
       <div className="modal">
           <h2>Planned Deliveries</h2>
           <ul>
-              {plannedDeliveries.map(delivery => (
+              {aggregatedDeliveryItems.map(delivery => (
                   <li key={(delivery.id)}>
                       <strong>{delivery.name} - {delivery.description}</strong>
                       <ul>
-                          {delivery.items.map((item) => (
-                              <li key={item.id}>{item.label} (GTIN: {item.gtin})</li>
+                          {delivery.items.map((item, index) => (
+                              <li key={index}>{item.label} (Quantity: {item.quantity})</li>
                           ))}
                       </ul>
                   </li>
