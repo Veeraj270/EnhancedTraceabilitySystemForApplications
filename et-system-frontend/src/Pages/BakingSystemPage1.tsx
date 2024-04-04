b1c16b65c80b05c1636366e23557ebbf1e5ecc14
josif <qc22435@bristol.ac.uk>
2024-04-04 17:23:46 +0000

    Started making the table.

--- et-system-frontend/src/Pages/BakingSystemPage1.tsx
+++ et-system-frontend/src/Pages/BakingSystemPage1.tsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./BakingSystemPage1Components/BS1Stylesheet.css"
 
 
 const BakingSystemPage1 = () => {
+
+    const [finalProductsData, setFinalProductsData] = useState([])
+
+    const fetchFinalProducts = async () => {
+        const response = await fetch("http://localhost:8080/api/customerorders/fetch-ordered-final-products")
+        if(!response.ok){
+            throw new Error("Error fetching ordered final products")
+        }
+        return await response.json();
+    }
+
+    useEffect(() => {
+        fetchFinalProducts().then((finalProductsData) => {
+            setFinalProductsData(finalProductsData)
+        })
+            .catch((reason) => {console.error("Error setting final products data." + reason)})
+    })
 
     return (
         <div className="BS1-page">
--- et-system-frontend/src/Pages/BakingSystemPage1Components/FinalProductsTable.tsx
+++ et-system-frontend/src/Pages/BakingSystemPage1Components/FinalProductsTable.tsx
@@ -1,4 +1,46 @@
-const FinalProductsTable = () => {
+import {useMemo} from "react";
+import {getCoreRowModel, useReactTable} from "@tanstack/react-table";
+import React from "react";
+
+const FinalProductsTable = ({finalProducts}) => {
+
+    const columns = useMemo(() => [
+        {
+            header: "Final product",
+            accessorKey: "finalProduct",
+        },
+        {
+            header: "Quantity",
+            accessorKey: "quantity"
+        },
+        {
+            header: "Associated order",
+            accessorKey: "associatedOrder"
+        },
+        {
+            header: "Add or Delete",
+            accessorKey: "addOrDelete"
+        }
+    ], [finalProducts])
+
+    const table = useReactTable({
+        data: finalProducts,
+        columns: columns,
+        getCoreRowModel: getCoreRowModel()
+    })
+
+    return (
+        <div>
+            <table>
+                <thead>
+
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        </div>
+    )
 
 }
 
