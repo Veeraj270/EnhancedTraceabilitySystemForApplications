e6f2a2d6a8fc27e4146747d07adc232b09e3f970
Matthew Cudby <oh22896@bristol.ac.uk>
2024-04-24 21:30:59 +0100

    Wrote barebones API for Baking System and did some file system restructuring

--- ETSystem/src/main/java/com/example/ETSystem/bakingSystem/BakingSystemAPI.java
+++ ETSystem/src/main/java/com/example/ETSystem/bakingSystem/BakingSystemAPI.java
@@ -1,5 +1,22 @@
 package com.example.ETSystem.bakingSystem;
 
-public class BakingSystemAPI {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+
+@RestController
+@RequestMapping(path = "api/baking-system")
+@CrossOrigin(origins = "http://localhost:3000")
+public class BakingSystemAPI {
+    private final BakingSystemService bakingSystemService;
+
+    @Autowired
+    public BakingSystemAPI(BakingSystemService bakingSystemService) {
+        this.bakingSystemService = bakingSystemService;
+    }
+
+    @PostMapping(path = "/process-bp-struct")
+    public void processBPStruct(@RequestBody BakingSystemService.BPStruct bpStruct) {
+        bakingSystemService.ProcessBPStruct(bpStruct);
+    }
 }
