8080719c8e4a4ac4fb3678616293aec3e1780635
Veeraj <ro22780@bris.ac.uk>
2024-05-01 12:05:47 +0100

    Begun changes to contracts to restyle it to be in the same style as CustomerOrders Page. Started by moving all interfaces to their own file.

--- et-system-frontend/src/Pages/ContractsPageComponents/ContractsModal.tsx
+++ et-system-frontend/src/Pages/ContractsPageComponents/ContractsModal.tsx
@@ -1,36 +1,6 @@
 import React, { useState } from 'react';
 
-type ModalProps = {
-    contract: Contract;
-    onClose: () => void;
-}
-
-type FinalProduct = {
-    id: number;
-    label: string;
-    cost: number;
-    quantity: number;
-};
-
-type CustomerOrder = {
-    id: number;
-    client: string;
-    date: string;
-    deliveryDate: string;
-    finalProducts: FinalProduct[];
-};
-
-type Contract = {
-    id: number;
-    client: string;
-    duration: string;
-    frequency: string;
-
-    finalProducts: FinalProduct[];
-
-    dates: Date[];
-
-};
+import "./ContractsInterfaces"
 
 const ContractsModal: React.FC<ModalProps> = ({ contract, onClose }) => {
     const [dates, setDates] = useState(contract.dates);
