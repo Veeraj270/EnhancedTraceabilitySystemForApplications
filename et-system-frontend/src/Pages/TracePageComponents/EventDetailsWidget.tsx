8ddd1e34dbca2cfbdf2c00d66483b15342860576
l-Luna <izzeldeen03@gmail.com>
2024-05-01 20:44:28 +0100

    include all information about event types on the traceability page

--- et-system-frontend/src/Pages/TracePageComponents/EventDetailsWidget.tsx
+++ et-system-frontend/src/Pages/TracePageComponents/EventDetailsWidget.tsx
@@ -1,21 +1,38 @@
-import React from "react";
-import {EventDetails} from "./Interfaces";
+import React, {ReactElement} from "react";
+import Event from "../Interfaces/Event";
 
-interface PropTypes {
-    eventDetails: EventDetails;
+interface PropTypes{
+    eventDetails: Event;
 }
 
-const EventDetailsWidget = (props: PropTypes) => {
-    const eventDetails = props.eventDetails;
-
-    return (
-        <div className={"TP-event-details-widget"}>
-            <h3>Event Details</h3>
-            <p><b>Timestamp:&nbsp;</b> {eventDetails.timestamp ? eventDetails.timestamp : "N/A"}</p>
-            <p><b>Event Type:&nbsp;</b> {eventDetails.type ? eventDetails.type : "N/A"}</p>
-            <p><b>Author:&nbsp;</b> {eventDetails.author ? eventDetails.author : "N/A"}</p>
+const renderEventSpecificDetails = (event: Event): ReactElement | undefined => {
+    if (event.data == undefined)
+        return undefined
+    if (event.type == "CreateEvent")
+        return <div>
+            <p><b>Creation Method:&nbsp;</b> {event.data.createType == "DELIVERED" ? "Delivery" : event.data.createType == "BAKED" ? "Baking" : "N/A"}</p>
+            <p><b>Location:&nbsp;</b> {event.data.location ?? "N/A"}</p>
         </div>
-    )
+    if (event.type == "MoveEvent")
+        return <p><b>Destination:&nbsp;</b> {event.data.destination ?? "N/A"}</p>
+    if (event.type == "UseEvent")
+        return <div>
+            <p><b>Location:&nbsp;</b> {event.data.location ?? "N/A"}</p>
+            <p><b>Quantity Used:&nbsp;</b> {event.data.quantityUsed ?? "N/A"}</p>
+        </div>
+    return undefined
+}
+
+const EventDetailsWidget = (props: PropTypes): ReactElement => {
+    const event = props.eventDetails;
+
+    return <div className={"TP-event-details-widget"}>
+        <h3>Event Details</h3>
+        <p><b>Timestamp:&nbsp;</b> {event.timestamp ?? "N/A"}</p>
+        <p><b>Event Type:&nbsp;</b> {event.type ?? "N/A"}</p>
+        <p><b>Author:&nbsp;</b> {event.userResponsible ?? "N/A"}</p>
+        {renderEventSpecificDetails(event)}
+    </div>
 }
 
 export default EventDetailsWidget;
